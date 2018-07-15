61aa4cc24b0b1ce0bec2c521a63405a8ee69bf1e
hermanho <hmcherman@gmail.com>
2018-07-16 03:57:09 +0800

    output result with drawing

--- detect_face.py
+++ detect_face.py
@@ -3,7 +3,7 @@ import os
 import copy
 import tensorflow as tf
 import numpy as np
-import facenet.src.align.detect_face as detect_face
+import facenet.src.align.detect_face as facenet_detect_face
 from facenet.src import facenet
 import imageio
 from PIL import Image, ImageFont, ImageDraw, ImageEnhance
@@ -21,7 +21,7 @@ def detect_faces(image_paths, image_size=160, margin=44):
         gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_memory_fraction)
         sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
         with sess.as_default():
-            pnet, rnet, onet = detect_face.create_mtcnn(sess, None)
+            pnet, rnet, onet = facenet_detect_face.create_mtcnn(sess, None)
 
     if isinstance(image_paths, str):
         image_paths = [image_paths]
@@ -31,7 +31,7 @@ def detect_faces(image_paths, image_size=160, margin=44):
     for image in tmp_image_paths:
 
         img = imageio.imread(os.path.expanduser(image), pilmode='RGB')
-        bounding_boxes, _ = detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+        bounding_boxes, points = facenet_detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
         if len(bounding_boxes) < 1:
             print("can't detect face, remove ", image)
         
@@ -43,10 +43,15 @@ def detect_faces(image_paths, image_size=160, margin=44):
             font_location = bounding_boxes[i,0:2] - np.array([0, 30])
             confidence = "{:.6f}".format(bounding_boxes[i,4] * 100)
             draw.text(font_location, str(confidence) + "%", fill="white", font=ImageFont.truetype("arial", 20))
+            for j in range(5):
+                point_x = points[j,i]
+                point_y = points[j+5,i]
+                r = 2
+                draw.ellipse((point_x-r, point_y-r, point_x+r, point_y+r), fill="lime")
         
         source_img.save(os.path.splitext(os.path.expanduser(image))[0] + "_result.jpg", "JPEG")
             
     return result_list
 
 if __name__ == '__main__':
-    detect_faces(sys.argv[1:])
+    detect_faces(['1.jpg'])
